adc8e0d0cafcab1350f11912240b2e87c4363901
farhadcu <fmfarhad231@gmail.com>
2024-05-23 19:58:28 +0600

    Contact Form Dynamic With Ajax -Class 23 part 4

--- js/main.js
+++ js/main.js
@@ -1,21 +1,6 @@
 $(document).ready(function () {
 
-    // elevate zoom activation 
-    $('.zoom-01').elevateZoom();
-
-    $('.zoom-02').elevateZoom({
-        zoomType: "inner",
-        cursor: "crosshair"
-    });
-
-    $('.zoom-03').elevateZoom({
-        zoomType: "lens",
-        lensShape: "round",
-        lensSize: 150,
-    });
-
-    //nice select jquery activation
-    $('select').niceSelect();
+  
 
 
 })
