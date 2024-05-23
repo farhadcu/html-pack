1b8d326d92a25e55fd3c0b77ac12c2a40f51d7cb
farhadcu <fmfarhad231@gmail.com>
2024-05-23 07:00:21 +0600

    ElevateZoom plugin - class 23 part 2

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 $(document).ready(function () {
 
+    // elevate zoom activation 
+    $('.zoom-01').elevateZoom();
+
+    $('.zoom-02').elevateZoom({
+        zoomType: "inner",
+        cursor: "crosshair"
+    });
+
+    $('.zoom-03').elevateZoom({
+        zoomType: "lens",
+        lensShape: "round",
+        lensSize: 150,
+    });
+
 
 })
